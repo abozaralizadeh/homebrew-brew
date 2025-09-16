adab7be29b770723704ff7e6708ff0bc30148218
Abozar <abozar.alizadeh@gmail.com>
2025-09-16 23:42:12 +0200

    Update upppdf.rb

--- upppdf.rb
+++ upppdf.rb
@@ -1,8 +1,8 @@
 class Upppdf < Formula
   desc "Unlock password protected PDF files using multiple methods"
   homepage "https://github.com/abozaralizadeh/UPPPDF"
-  url "https://github.com/abozaralizadeh/UPPPDF/archive/refs/tags/master.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  url "https://github.com/abozaralizadeh/UPPPDF/archive/refs/tags/v1.0.9.tar.gz"
+  sha256 "6042e6a22b001050b705e56aec93112e5ae7cb2ebfd5db56615796dd69a41514"
   license "MIT"
 
   head "https://github.com/abozaralizadeh/UPPPDF.git", branch: "master"
